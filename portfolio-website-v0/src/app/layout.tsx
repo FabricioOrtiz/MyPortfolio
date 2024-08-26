4b2722ed7be6d364b6054cdcda0c84e432492a23
Tito Ortiz CR <fabricio.ortiz.alfaro@gmail.com>
2024-08-26 15:22:27 -0600

    Initial commit

---
+++ portfolio-website-v0/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'David | Creative Developer',
+  description: 'Portfolio of David Heckhoff - Creative developer living in Germany - Focused on WebGL and ReactJS',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
